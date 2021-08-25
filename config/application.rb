3850ddf5c24dce776253d1a71b34bfce353fab1b
satoshi naruki <maptjgpatmpgg1113@gmail.com>
2021-08-26 01:34:23 +0900

    土台作成

---
+++ config/application.rb
@@ -0,0 +1,19 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Instaclone
+  class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.load_defaults 5.2
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
+  end
+end
