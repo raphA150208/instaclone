040616e88e78d4104d8a64174c91cf6252de50dd
satoshi naruki <maptjgpatmpgg1113@gmail.com>
2021-08-26 14:23:23 +0900

    アソシエーション設定

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -21,7 +21,7 @@ class PicturesController < ApplicationController
 
   # POST /pictures or /pictures.json
   def create
-    @picture = Picture.new(picture_params)
+    @picture = current_user.pictures.build(picture_params)
     if params[:back]
       render :new
     else
@@ -59,7 +59,7 @@ class PicturesController < ApplicationController
     end
   end
   def confirm
-    @picture = Picture.new(picture_params)
+    @picture = current_user.pictures.build(picture_params)
     render :new if @picture.invalid?
   end
 
