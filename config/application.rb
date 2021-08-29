65f7aad1801c9c27db565ed33da1c6a62e4be47f
satoshi naruki <maptjgpatmpgg1113@gmail.com>
2021-08-30 05:09:05 +0900

    辞書ファイルでの日本語化

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,10 @@
-require_relative 'boot'
-
-require 'rails/all'
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+require_relative "boot"
+require "rails/all"
 Bundler.require(*Rails.groups)
 
 module Instaclone
   class Application < Rails::Application
-    config.time_zone = 'Tokyo'
+    config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
     config.load_defaults 5.2
     config.generators do |g|
